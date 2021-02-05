b91b5881e1a9694f7834d265850403f3eed41bdb
Wie Lie Sie <wlie.sie@gmail.com>
2021-02-04 20:42:40 -0500

    fixed encipher with proper math.mod()

--- js/affine.js
+++ js/affine.js
@@ -1,29 +1,26 @@
 function encipher(multiplicativeKey, additiveKey, plainText)
 {
-    if (multiplicativeKey > 25 || multiplicativeKey < 1 || multiplicativeKey % 2 == 0 || multiplicativeKey == 13) {
-        return "Please enter a valid multiplicative key (odd number between 1 to 25 not including 13)"
-    } else {
-        var stringInput = plainText.toString();
-        stringInput = stringInput.replace(/\W/ig, "").toUpperCase();
-        var chars = stringInput.split("");
-        
-        for (i = 0; i < chars.length; i++)
-        {
-            var temp = chars[i].charCodeAt(0) - 64;
-            var multiplicativeValue = (temp * multiplicativeKey) % 26;
-
-            if (multiplicativeValue == 0) 
-                multiplicativeValue += 26;
-            if (additiveKey < 0)  
-                additiveKey = (additiveKey % 26) + 26;
-
-            var finalValue = (multiplicativeValue + additiveKey) % 26;
-
-            if (finalValue == 0)
-                finalValue = 26;
-
-            chars[i] = String.fromCharCode(finalValue + 64);
-        }
-        return chars.join("").replace(/(.{5})/g, '$1 ');
+    multiplicativeKey = math.mod(multiplicativeKey, 26);
+    additiveKey = math.mod(additiveKey, 26);
+    if (multiplicativeKey % 2 == 0 || multiplicativeKey == 13) {
+        return null;
     }
+       
+    var stringInput = plainText.toString();
+    stringInput = stringInput.replace(/\W/ig, "").toUpperCase();
+    var chars = stringInput.split("");
+        
+    for (i = 0; i < chars.length; i++)
+    {
+        var temp = chars[i].charCodeAt(0) - 64;
+        var multiplicativeValue = math.mod((temp * multiplicativeKey), 26);
+        if (multiplicativeKey == 0) multiplicativeKey = 26;
+        var finalValue = math.mod((multiplicativeValue + additiveKey), 26);
+
+        if (finalValue == 0)
+            finalValue = 26;
+
+        chars[i] = String.fromCharCode(finalValue + 64);
+    }
+    return chars.join("")   //.replace(/(.{5})/g, '$1 ') -> add whitespace after each 5th element
 }
